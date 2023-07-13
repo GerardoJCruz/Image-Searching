9020691a2e420e53642c2e6f716fdccdfd77427f
Gerardo J Cruz <gerardojcruz98@gmail.com>
2023-07-12 21:17:56 -0600

    Project ready to deploy

--- index.js
+++ index.js
@@ -5,17 +5,26 @@ const pagerDiv = document.getElementById('pager');
 
 const api_key = '18188601-534a756987856a27626a98f10';
 const maxPerPage = 20;
-let currentPage = 1;
+let searching_term = '';
+let currentPage;
 
 // Handle the form submission
 myForm.addEventListener('submit', function (e) {
-    getImages(e);
+    e.preventDefault();
+
+    // Validate that the Form is not empty
+    searching_term = e.target.searching.value;
+    if(searching_term === ''){
+        showAlert('provide a search term');
+        return;
+    }
+
+    currentPage = 1;
+    getImages(searching_term);
 });
 
 // Function to fetch images from Pixabay API
-const getImages = (event) => {
-    event.preventDefault();
-    const searching_term = event.target.searching.value;
+const getImages = (searching_term) => {
     const url = `https://pixabay.com/api/?key=${api_key}&per_page=${maxPerPage}&q=${searching_term}&page=${currentPage}`;
 
     fetch(url)
@@ -26,9 +35,12 @@ const getImages = (event) => {
 
             // Calling displayImages
             displayImages(data);
+            
 
             //To calc the number of pages
-            displayPages( data.total, currentPage, maxPerPage);
+            displayPages(data.total, maxPerPage);
+
+            // listeningForPages(searching_term);
         })
         .catch(error => {
             // Heandle erros
@@ -38,14 +50,13 @@ const getImages = (event) => {
 
 // Function to display images
 const displayImages = (data) => {
-
     data.hits.forEach(e => {
         // Create the images container and add css classes
         const imagesCard = document.createElement('div');
         imagesCard.classList.add('card', 'rounded-0');
 
         // Extract card information
-        const { id, largeImageURL, previewURL, likes, views } = e
+        const { largeImageURL, previewURL, likes, views } = e
 
         // Create the individual cards
         const htmlBlock = `
@@ -67,35 +78,57 @@ const displayImages = (data) => {
 }
 
 //Function to remove the previous images.
-const cleaner = ()=>{
-    while(imagesContainer.firstChild){
+const cleaner = () => {
+    while (imagesContainer.firstChild) {
         imagesContainer.removeChild(imagesContainer.firstChild);
     }
 }
 
 
 // Function to display pages
-function displayPages(total, current, maxImages) {
-    const totalPages = Math.ceil(total / maxImages);
-    const ul = document.createElement('ul');
-    ul.classList.add('pager__links', 'list-unstyled', 'd-flex', 'gap-3', 'justify-content-center', 'mt-5')
+function displayPages(total, maxImages) {
+    pagesCleaner();
+    const totalPages = Math.ceil( total> 400? 20 : total / maxImages);
+    
+    // Create a tags and add it to the HTML
+    for (i = 0; (i < totalPages); i++) {
+        const pageLink = document.createElement('a');
+        pageLink.classList.add('text-decoration-none')
+        pageLink.dataset.page = i+1;
+        pageLink.innerHTML = `${i+1}`;
 
-    // Create li tags and add it to the HTML
-    for(i = 0; i < totalPages; i++){
-        const list = document.createElement('li');
-        list.innerHTML = i+1;
-        ul.appendChild(list);
+        //Add the action to change the page
+        pageLink.onclick = () =>{
+            currentPage = pageLink.dataset.page;
+            getImages(searching_term);
+        }
+
+        pagerDiv.appendChild(pageLink);
     }
-    pagerDiv.appendChild(ul);
-    // listeningForPages(ul);
 }
 
-// const listeningForPages= () =>{
-//     const listLinks = unOlist.getElementsByTagName('li');
-//     for(let i = 0; i < listLinks.length; i++){
-//         listLinks[i].addEventListener('click', function(e){
-//             currentPage = i+1;
-//             getImages(e);
-//         })
-//     }
-// }
+const pagesCleaner = () => {
+    while (pagerDiv.firstChild) {
+        pagerDiv.removeChild(pagerDiv.firstChild);
+    }
+}
+
+
+// Function to show alert
+function showAlert(message){
+    const existingAlert = document.querySelector('.bg-danger');
+    if(!existingAlert){
+        const alert = document.createElement('p');
+        alert.classList.add('bg-danger', 'text-white', 'px-4', 'py-3', 'rounded', 'm-auto', 'mt-5', 'text-center');
+        alert.innerHTML= `
+            <strong class="fw-bold">Error!</strong>
+            <span class="block">${message}</span>
+        `;
+
+        myForm.appendChild(alert);
+
+        setTimeout(() =>{
+            alert.remove();
+        }, 5000);
+    }
+}
